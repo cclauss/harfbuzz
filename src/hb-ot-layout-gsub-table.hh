85e79df288b9cbba17a8df34472ec008d2d7b6d1
Behdad Esfahbod <behdad@behdad.org>
2018-12-27 09:56:41 -0500

    Change hb_void_t implementation

--- src/hb-ot-layout-gsub-table.hh
+++ src/hb-ot-layout-gsub-table.hh
@@ -1304,7 +1304,7 @@ struct SubstLookup : Lookup
   {
     TRACE_CLOSURE (this);
     if (!c->should_visit_lookup (this_index))
-      return_trace (HB_VOID);
+      return_trace (hb_void_t ());
 
     c->set_recurse_func (dispatch_closure_recurse_func);
 
@@ -1406,7 +1406,7 @@ struct SubstLookup : Lookup
   static hb_closure_context_t::return_t dispatch_closure_recurse_func (hb_closure_context_t *c, unsigned int lookup_index)
   {
     if (!c->should_visit_lookup (lookup_index))
-      return HB_VOID;
+      return hb_void_t ();
 
     hb_closure_context_t::return_t ret = dispatch_recurse_func (c, lookup_index);
 
